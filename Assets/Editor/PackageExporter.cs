aa33823a128b2d8dd45c48d6b35812a38e33e988
Yoshifumi Kawai <ils@neue.cc>
2019-05-20 02:12:11 +0900

    plus package

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class PackageExporter
+{
+    [MenuItem("Tools/Export Unitypackage")]
+    public static void Export()
+    {
+        // configure
+        var root = "UniRx.Async";
+        var exportPath = "./UniRx.Async.unitypackage";
+
+        var path = Path.Combine(Application.dataPath, root);
+        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+            .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".asmdef" || Path.GetExtension(x) == ".json")
+            .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+            .ToArray();
+
+        UnityEngine.Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+
+        AssetDatabase.ExportPackage(
+            assets,
+            exportPath,
+            ExportPackageOptions.Default);
+
+        UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+    }
+}
+
+#endif
