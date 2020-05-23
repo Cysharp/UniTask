6dfb9690151ed533168be9144603e2839af84923
neuecc <ils@neue.cc>
2020-05-24 01:30:52 +0900

    Add ValueTask.AsUniTask only for .NET Core

--- src/UniTask.NetCore/NetCore/UniTask.AsValueTask.cs
+++ src/UniTask.NetCore/NetCore/UniTask.AsValueTask.cs
@@ -15,5 +15,16 @@ namespace Cysharp.Threading.Tasks
         {
             return task;
         }
+
+        public static UniTask<T> AsUniTask<T>(this ValueTask<T> task, bool useCurrentSynchronizationContext = true)
+        {
+            // NOTE: get _obj and _token directly for low overhead conversion but not yet implemented.
+            return task.AsTask().AsUniTask(useCurrentSynchronizationContext);
+        }
+
+        public static UniTask AsUniTask(this ValueTask task, bool useCurrentSynchronizationContext = true)
+        {
+            return task.AsTask().AsUniTask(useCurrentSynchronizationContext);
+        }
     }
 }
